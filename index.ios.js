e2d03657b3647f624bc34fb022de010dff1db938
olegsugatov <oleg.sugatov@gmail.com>
2017-09-13 23:43:01 +0300

    +positioning

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  ScrollView,
   Text,
   View,
   Image
@@ -15,47 +10,87 @@ import {
 
 export default class rnGuide extends Component {
   render() {
+    
     let pic = {
         uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
+
     return (
-    // Text
-    <View style={styles.container}>
-      
+
+      <View style={styles.container}>
+
+      {/* Text */}
       <View>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text>Hello{this.props.name} !</Text> 
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
       </View>
 
+      {/* Image */}
       <View>
         <Image source={pic} style={{width: 193, height: 110}}/>
       </View>
 
-      <View style={styles.lilbox}/>
-      <View style={styles.medbox}/>
-      <View style={styles.bigbox}/>
+      {/* Props in Action */}
+      <View>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
 
-      <View style={styles.lilboxflex}/>
-      <View style={styles.medboxflex}/>
-      <View style={styles.bigboxflex}/>
-    </View>
+      {/* Width / Height */}
+        <View style={styles.lilbox}/>
+        <View style={styles.medbox}/>
+        <View style={styles.bigbox}/>
+
+        {/*Positioning*/}
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+
+
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+
+
+
+
+
+      </View>
+
     );
   }
 }
 
-class LotsOfGreeting extends Component {
-    render() {
-        <View style={{alignItem: 'center'}}>
-            <Greeting name='Rexxar' />
-            <Greeting name='Jaina' />
-            <Greeting name='Valeera' />
-        </View>
-    }
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -77,29 +112,29 @@ const styles = StyleSheet.create({
   },
   lilbox: {
     width: 50,
-    height: 50,
+    height: 10,
     backgroundColor: 'powderblue'
   },
   medbox: {
     width: 100,
-    height: 100,
+    height: 10,
     backgroundColor: 'skyblue'
   },
   bigbox: {
     width: 150,
-    height: 150,
+    height: 10,
     backgroundColor: 'steelblue'
   },
 
-  // lilboxflex: {
+  // lilboxf: {
   //   flex: 1,
   //   backgroundColor: 'skyblue'
   // },
-  // medboxflex: {
+  // medboxf: {
   //   flex: 2,
   //   backgroundColor: 'skyblue'
   // },
-  // bigboxflex: {
+  // bigboxf: {
   //   flex:3,
   //   backgroundColor: 'steelblue'
   // }
